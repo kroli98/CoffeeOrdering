e4c54afdd976d18bffdd4cd66f541b98227d1e5a
kroli98 <roli25321@gmail.com>
2024-02-07 11:36:57 +0100

    Initial Commit

---
+++ CoffeeOrdering/CoffeeOrderingApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeOrderingApp.swift
+//  CoffeeOrdering
+//
+//  Created by Kiss Roland on 07/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeOrderingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
