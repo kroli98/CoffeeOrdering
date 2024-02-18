36aaa2c0521c0d6ac31665fba0f213cc332c9ec7
kroli98 <roli25321@gmail.com>
2024-02-18 14:27:53 +0100

    orderlistview added

---
+++ CoffeeOrdering/View Models/OrderListViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  OrderListViewModel.swift
+//  CoffeeOrdering
+//
+//  Created by Kiss Roland on 07/02/2024.
+//
+
+import Foundation
---
+++ CoffeeOrdering/Views/OrderListView.swift
@@ -0,0 +1,18 @@
+//
+//  OrderListView.swift
+//  CoffeeOrdering
+//
+//  Created by Kiss Roland on 17/02/2024.
+//
+
+import SwiftUI
+
+struct OrderListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OrderListView()
+}
